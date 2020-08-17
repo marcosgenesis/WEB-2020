1d544923560288e33d4032ea616a430833a2feea
marcosgenesis <marcosgenesisof@gmail.com>
2020-08-17 18:22:05 -0300

    ✅ Atividade 04

--- atividade-3/src/pages/ListPage/index.js
+++ atividade-3/src/pages/ListPage/index.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
+import { FiEdit, FiTrash } from "react-icons/fi";
 import api from "../../services/api";
-import { ListGroup } from "react-bootstrap";
+import { ListGroup, Modal, Button } from "react-bootstrap";
 function ListPage() {
   const [disciplinas, setDisciplinas] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [id, setId] = useState("");
+  const [nome, setNome] = useState("");
+  const [curso, setCurso] = useState("");
+  const [qtdAlunos, setQtdAlunos] = useState(0);
+
   useEffect(() => {
     async function carregarDisciplinas() {
       const response = await api.get("/disciplinas");
@@ -11,17 +18,113 @@ function ListPage() {
     }
     carregarDisciplinas();
   }, []);
+
+  async function handleOpen(disciplina) {
+    setOpen(true);
+    setId(disciplina.id);
+    setQtdAlunos(disciplina.quantidade);
+    setNome(disciplina.nome);
+    setCurso(disciplina.curso);
+  }
+  async function handleEdit() {
+    await api.put(`/disciplinas/${id}`, { nome, curso, quantidade: qtdAlunos });
+    setOpen(false);
+
+    const findIndex = disciplinas.findIndex(
+      (disciplina) => disciplina.id === id
+    );
+    const disciplinasEditadas = disciplinas;
+    disciplinasEditadas[findIndex] = { id, nome, curso, quantidade: qtdAlunos };
+    console.log(disciplinasEditadas);
+    setDisciplinas(disciplinasEditadas);
+
+    setNome("");
+    setId(null);
+    setCurso("");
+    setQtdAlunos("");
+  }
+
+  async function handleDelete(disciplina) {
+    await api.delete(`/disciplinas/${disciplina.id}`);
+    const disciplinaUpdated = disciplinas.filter(
+      (disciplinaItem) => disciplinaItem.id !== disciplina.id
+    );
+    setDisciplinas(disciplinaUpdated);
+  }
+
   return (
     <div style={{ width: "100%" }}>
       <ListGroup variant="flush">
         {disciplinas.map((disciplina) => (
-          <ListGroup.Item>
-            <h4>{disciplina.nome}</h4>
-            <p>{disciplina.curso}</p>
-            <p>Quantidade de alunos: {disciplina.quantidade}</p>
-          </ListGroup.Item>
+          <>
+            <ListGroup.Item>
+              <span
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <h4>{disciplina.nome}</h4>
+                <FiEdit
+                  style={{
+                    marginLeft: "10px",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => handleOpen(disciplina)}
+                />
+                <FiTrash
+                  style={{
+                    marginLeft: "10px",
+                    cursor: "pointer",
+                  }}
+                  onClick={() => handleDelete(disciplina)}
+                />
+              </span>
+              <p>{disciplina.curso}</p>
+              <p>Quantidade de alunos: {disciplina.quantidade}</p>
+            </ListGroup.Item>
+          </>
         ))}
       </ListGroup>
+      {open && (
+        <Modal.Dialog>
+          <Modal.Header closeButton={false}>
+            <Modal.Title>Editar </Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <form>
+              <input
+                type="text"
+                placeholder="Nome"
+                defaultValue={nome}
+                onChange={(e) => setNome(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Curso"
+                defaultValue={curso}
+                onChange={(e) => setCurso(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Quantidade de alunos"
+                defaultValue={qtdAlunos}
+                onChange={(e) => setQtdAlunos(e.target.value)}
+              />
+            </form>
+          </Modal.Body>
+
+          <Modal.Footer>
+            <Button variant="secondary" onClick={() => setOpen(false)}>
+              Fechar
+            </Button>
+            <Button variant="primary" onClick={handleEdit}>
+              Salvar Mudanças
+            </Button>
+          </Modal.Footer>
+        </Modal.Dialog>
+      )}
     </div>
   );
 }
